f480e297538f960a6fc7cde69fdb37acaf5871f5
John Wells <molekewl2017@gmail.com>
2022-02-24 20:30:00 -0800

    add event listeners & twitter api to post the quote

--- script.js
+++ script.js
@@ -2,18 +2,30 @@
 let apiQuotes = [];
 let randomQuote = [];
 
+const twitter = document.getElementById('twitter');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const button = document.getElementById('new-quote');
+
 // Random new quote
 function newQuote() {
   randomQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  if (randomQuote.text.length > 50) {
+    quote.classList.add('long-quote');
+  } else {
+    quote.classList.remove('long-quote');
+  }
+  quote.textContent = randomQuote.text;
 }
 
 async function getQuotes() {
   try {
-    const response = await fetch('https://type.fit/api/quotes')
+    const response = await fetch('https://type.fit/api/quotes');
     apiQuotes = await response.json();
 
     newQuote();
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   }
 }
@@ -21,12 +33,21 @@ async function getQuotes() {
 // On Load
 getQuotes();
 
-const quote = document.getElementById('quote');
-const author = document.getElementById('author');
-const button = document.getElementById('new-quote');
-
 button.addEventListener('click', () => {
   quote.innerText = randomQuote.text;
-  author.innerText = randomQuote.author;
-  newQuote()
+  if (!randomQuote.author) {
+    author.textContent = 'Unknown';
+  } else {
+    author.textContent = randomQuote.author;
+  }
+  newQuote();
+});
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`
+  window.open(twitterUrl, '_blank');
+}
+
+twitter.addEventListener('click', () => {
+  tweetQuote();
 });
